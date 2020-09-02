4131f83ab452a5dfad51389f575d0d7ac5af5fb7
lichangtong <18513781024@163.com>
2020-09-02 16:55:11 +0800

    spring alibaba  nacos feignclient gateway demo

--- alibaba-nacos-gateway/src/main/java/com/alibaba/demo/filter/FluidControlGlobalGatewayFilter.java
+++ alibaba-nacos-gateway/src/main/java/com/alibaba/demo/filter/FluidControlGlobalGatewayFilter.java
@@ -1,20 +1,77 @@
 package com.alibaba.demo.filter;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Component
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//@Component
+@Slf4j
 public class FluidControlGlobalGatewayFilter implements GlobalFilter, Ordered {
+    /**
+     * 去掉空格,换行和制表符
+     *
+     * @param str
+     * @return
+     */
+    private static String formatStr(String str) {
+        if (str != null && str.length() > 0) {
+            Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+            Matcher m = p.matcher(str);
+            return m.replaceAll("");
+        }
+        return str;
+    }
+
+    /**
+     * 读取body内容
+     *
+     * @param serverHttpRequest
+     * @return
+     */
+    public static String resolveBodyFromRequest(ServerHttpRequest serverHttpRequest) {
+        //获取请求体
+        StringBuilder sb = new StringBuilder();
+        serverHttpRequest.getBody().subscribe(buffer -> {
+            byte[] bytes = new byte[buffer.readableByteCount()];
+            buffer.read(bytes);
+            DataBufferUtils.release(buffer);
+            String bodyString = new String(bytes, StandardCharsets.UTF_8);
+            sb.append(bodyString);
+        });
+        return formatStr(sb.toString());
+    }
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         //        请求信息
+
         ServerHttpRequest request = exchange.getRequest();
+        InetSocketAddress inetSocketAddress = request.getRemoteAddress();
+        String hostAddress = inetSocketAddress.getAddress().getHostAddress();
+        String hostName = inetSocketAddress.getHostName();
+        log.info("请求地址IP,{}, hostName:{}", hostAddress, hostName);
+        String methodValue = request.getMethodValue();
+        log.info("method:{}", methodValue);
+
+
+        String body = resolveBodyFromRequest(exchange.getRequest());
+        System.out.println(body);
+        log.info("body:{}",body);
+        MultiValueMap<String, String> multiValueMap = request.getQueryParams();
+        System.out.println(JSON.toJSONString(multiValueMap));
         //        返回信息
         ServerHttpResponse response = exchange.getResponse();
         return chain.filter(exchange);
--- alibaba-nacos-payment-service/src/main/java/com/alibaba/demo/controller/PaymentController.java
+++ alibaba-nacos-payment-service/src/main/java/com/alibaba/demo/controller/PaymentController.java
@@ -2,21 +2,26 @@ package com.alibaba.demo.controller;
 
 import com.alibaba.demo.entity.User;
 import com.alibaba.demo.feign.ConsumerService;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/pay")
-@EnableAutoConfiguration
+@Slf4j
 public class PaymentController {
 
     @Autowired
     private ConsumerService consumerService;
     @PostMapping(value = "/payment")
-    public String getPayment() {
+    public String getPayment(@RequestBody @Valid User user) {
+        log.info("前端请求参数：{}", JSON.toJSONString(user));
        /* Balance  balance = new Balance(100L,2L,2222222L,"你好，这是个测试");
         return JSON.toJSONString(balance);*/
 
